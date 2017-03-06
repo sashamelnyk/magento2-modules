1245e1a1e51989ab02d3e47c2262f9928f36e8e0
sashamelnyk <sashamelnyk.lds@gmail.com>
2017-03-06 08:21:48 +0200

    Module 4

--- Training/EavBackend/Setup/UpgradeData.php
+++ Training/EavBackend/Setup/UpgradeData.php
@@ -3,13 +3,17 @@
 namespace Training\EavBackend\Setup;
 
 use Magento\Catalog\Model\Product;
+use Magento\Customer\Model\Customer;
 use Magento\Eav\Model\Entity\Attribute\Backend\ArrayBackend;
 use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
 use Magento\Eav\Model\Entity\Attribute\Source\Table as TableSourceModel;
 use Magento\Eav\Setup\EavSetup;
+use Magento\Catalog\Setup\CategorySetupFactory;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Customer\Setup\CustomerSetup;
+use Magento\Customer\Setup\CustomerSetupFactory;
 
 class UpgradeData implements UpgradeDataInterface
 {
@@ -17,10 +21,20 @@ class UpgradeData implements UpgradeDataInterface
      * @var EavSetup
      */
     private $eavSetup;
+    private $catalogSetupFactory;
+    private $customerSetupFactory;
+    //private $storeManager;
 
-    public function __construct(EavSetup $eavSetup)
+    public function __construct(
+	EavSetup $eavSetup, 
+	CategorySetupFactory $categorySetupFactory,
+	CustomerSetupFactory $customerSetupFactory
+    )
     {
         $this->eavSetup = $eavSetup;
+	$this->catalogSetupFactory = $categorySetupFactory;
+	$this->customerSetupFactory = $customerSetupFactory;
+	//$this->storeManager = $storeManager;
     }
 
     /**
@@ -59,5 +73,34 @@ class UpgradeData implements UpgradeDataInterface
                 ],
             ]);
         }
+        if (version_compare('0.1.5', $context->getVersion(), '>=')) {
+		$catalogSetup = $this->catalogSetupFactory->create(['setup' => $setup]);
+		$catalogSetup->updateAttribute(
+			Product::ENTITY,
+			'compatible_condiments',
+			[
+				'frontend_model' => \Training\EavBackend\Entity\Attribute\Frontend\HtmlList::class,
+				'is_html_allowed_on_front' => 1,
+			]
+		);
+        }
+	if (version_compare($context->getVersion(), '0.2.0', '<')) {
+		/** @var CustomerSetup $customerSetup */
+		$customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+		$customerSetup->addAttribute(
+			Customer::ENTITY,
+			'priority',
+			[
+				'label' => 'Priority',
+				'type' => 'int',
+				'input' => 'select',
+				'source' => \Training\EavBackend\Entity\Attribute\Source\CustomerPriority::class,
+				'required' => 0,
+				'system' => 0,
+				'position' => 100
+			]
+		);
+		$customerSetup->getEavConfig()->getAttribute('customer', 'priority')->setData('used_in_forms', ['adminhtml_customer'])->save();
+	}
     }
 }
---
+++ Training/Repository/Controller/Repository/Product.php
@@ -0,0 +1,70 @@
+<?php
+namespace Training\Repository\Controller\Repository;
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+//use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+
+class Product extends Action
+{
+	/**
+	* @var ProductRepositoryInterface
+	*/
+	private $productRepository;
+	/**
+	* @var SearchCriteriaBuilder
+	*/
+	private $searchCriteriaBuilder;
+	//private $filterBuilder;
+
+	public function __construct(
+		Context $context,
+		ProductRepositoryInterface $productRepository,
+		SearchCriteriaBuilder $searchCriteriaBuilder,
+		FilterBuilder $filterBuilder
+	) {
+		parent::__construct($context);
+		$this->productRepository = $productRepository;
+		$this->searchCriteriaBuilder = $searchCriteriaBuilder;
+		$this->filterBuilder = $filterBuilder;
+	}
+
+	public function execute()
+	{
+		$this->getResponse()->setHeader('Content-Type', 'text/plain');
+		$products = $this->getProductsFromRepository();
+		foreach ($products as $product) {
+			$this->outputProduct($product);
+		}
+	}
+	/**
+	* @return ProductInterface[]
+	*/
+	private function getProductsFromRepository()
+	{
+		$this->setProductTypeFilter();
+		$criteria = $this->searchCriteriaBuilder->create();
+		$products = $this->productRepository->getList($criteria);
+		return $products->getItems();
+	}
+	private function outputProduct(ProductInterface $product)
+	{
+		$this->getResponse()->appendBody(sprintf(
+		"%s - %s (%d)\n",
+		$product->getName(),
+		$product->getSku(),
+		$product->getId())
+		);
+	}
+	private function setProductTypeFilter()
+	{
+		$configProductFilter = $this->filterBuilder
+			->setField('type_id')
+			->setValue(ConfigurableProduct::TYPE_CODE)
+			->setConditionType('eq')
+			->create();
+		$this->searchCriteriaBuilder->addFilter([$configProductFilter]);
+	}
+}
